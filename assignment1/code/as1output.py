1ca0ad31a6b524a9c70d499258493e3015f98acf
Aryan Sharan Reddy <102508424+1Aryan8@users.noreply.github.com>
2022-04-22 12:19:00 +0530

    Create as1output.py

---
+++ assignment1/code/as1output.py
@@ -0,0 +1,10 @@
+x=90
+Angle_BAC=x
+#angle in a semicircle is 90
+Angle_ABC=90-(x)/2
+#Angle_BAC+Angle_ABC+Angle_ACB=180
+#Angle_ABC=Angle_ACB
+#In a cyclic quadrilateral, opposite angles are always supplementary
+Angle_ADC=180-(Angle_ABC)
+y=Angle_ADC
+print("y=",y,chr(176),sep='')
